c9fb6f4a492c001306fb8b8da3b967c7143d534c
Brutaler <juandpedraza333@gmail.com>
2025-07-24 20:27:54 -0500

    player physics rig almost complete

---
+++ Assets/scrips/PhysicRig.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PhysicRig : MonoBehaviour
+{
+    public Transform playerHead;
+    public Transform leftHandController;
+    public Transform rightHandController;    
+
+    public ConfigurableJoint headJoint;
+    public ConfigurableJoint leftHandJoint;
+    public ConfigurableJoint rightHandJoint;
+
+    public CapsuleCollider bodyCollider;
+
+
+    public float bodyHeightMin = 0.5f;
+    public float bodyHeightMax = 2;
+
+    void FixedUpdate()
+    {
+        bodyCollider.height = Mathf.Clamp(playerHead.localPosition.y, bodyHeightMin, bodyHeightMax);
+        bodyCollider.center = new Vector3(playerHead.localPosition.x, bodyCollider.height/2, playerHead.localPosition.z);
+
+        leftHandJoint.targetPosition = leftHandController.localPosition;
+        leftHandJoint.targetRotation = leftHandController.localRotation;
+
+        rightHandJoint.targetPosition = rightHandController.localPosition;
+        rightHandJoint.targetRotation = rightHandController.localRotation;
+
+        headJoint.targetPosition = playerHead.localPosition;
+    }
+}
